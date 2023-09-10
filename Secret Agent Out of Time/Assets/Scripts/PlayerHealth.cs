7c0a9a5d10da01a9b6d595af98b1bfd123bb29ca
Yachiryu <daniel.castrillon.a@gmail.com>
2023-09-10 15:01:35 -0500

    Incorporacion elementos varios

---
+++ Secret Agent Out of Time/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Elementos vida")]
+    public float health;
+    public float maxHealth;
+    public Image healthBar;
+    [SerializeField] private GameObject muerte;
+    [SerializeField] private GameObject deathpanel;
+
+    public GameObject torretas;
+
+    private void Start()
+    {
+        maxHealth = health;        
+    }
+
+    private void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Instantiate(muerte, transform.position, Quaternion.identity);
+        //deathpanel.SetActive(true);
+        Destroy(torretas);
+        Destroy(gameObject); 
+
+    }
+}
