cdf6cddc7b096e5a2a4e441aeda656d9c881ecb5
Delluxxe <jesusarrietaherazo@gmail.com>
2023-09-13 20:59:56 -0500

    Sprites_lvl_nave

--- Secret Agent Out of Time/Assets/Assets/Scripts/Mov_Parallax.cs
+++ Secret Agent Out of Time/Assets/Assets/Scripts/Mov_Parallax.cs
@@ -7,15 +7,17 @@ public class Mov_Parallax : MonoBehaviour
     [SerializeField] private Vector2 velocidadMovimiento;
     private Vector2 offset;
     private Material material;
+    private Rigidbody2D jugadorRB;
 
     private void Awake()
     {
         material = GetComponent<SpriteRenderer>().material;
+        jugadorRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        offset = velocidadMovimiento * Time.deltaTime;
+        offset = (jugadorRB.velocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
         material.mainTextureOffset += offset;
     }
 }
